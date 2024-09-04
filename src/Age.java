02a50ec0672a25956f0f45d3421cf955a9d1cc6c
artjom2281337 <artjomsmorgulenko@gmail.com>
2024-09-04 13:00:48 +0300

    Create Age.java

---
+++ src/Age.java
@@ -0,0 +1,33 @@
+public class Age {
+    public static void main(String[] args) throws Exception {
+        int age = 45;
+            if(age == 18) {
+                System.out.println("You can drive a car.");
+            
+            }
+ 
+ 
+                        if(age == 10 || age == 20 || age == 30 || age == 40 || age == 50 || age == 60 ||
+                           age == 70 || age == 80 || age == 90 || age == 100 || age == 110 || age == 120) {
+                            System.out.println("Anniversary Party!!");
+                        }
+
+
+
+                    if(age == 100) {
+                        System.out.println("Congratulations!!!");
+                        System.out.println("Congratulations!!!");
+                        System.out.println("Congratulations!!!");
+                    }
+
+
+
+
+                                if( age > 40 && age < 50) {
+                                    System.out.println("Happy mid-life!");
+                                }
+
+            System.out.println("Press space to exit.");
+
+    }
+}
