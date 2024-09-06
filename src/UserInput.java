da5ab39672f24a276e79f866c228adeb43b60a63
artjom2281337 <artjomsmorgulenko@gmail.com>
2024-09-06 09:04:35 +0300

    Create UserInput.java

---
+++ src/UserInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class UserInput {
+    public static void main(String[] args) throws Exception {
+    
+        Scanner in = new Scanner(System.in);
+        
+            System.out.println("soglasni uznali");
+
+
+
+
+    }
+}
