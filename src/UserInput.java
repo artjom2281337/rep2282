f8074b3cb7eb37883636f7c903cd691525dca1b3
artjom2281337 <artjomsmorgulenko@gmail.com>
2024-09-06 09:21:11 +0300

    Update UserInput.java

--- src/UserInput.java
+++ src/UserInput.java
@@ -3,16 +3,28 @@ public class UserInput {
     public static void main(String[] args) throws Exception {
     
         Scanner in = new Scanner(System.in);
+
+        int result = 0;
         
             String input = "";
+            
+                // Integer values
 
-                System.out.println("Type somethingd");
-        
-        //input = "Hello";
+        System.out.println("10x Multiplier");
 
         input = in.nextLine();
 
-        System.out.println("You typed " + input);
+        result = Integer.parseInt(input) * 10;     // Converter from String to int
+
+        System.out.println(result);
+
+        //         System.out.println("Type somethingd");
+        
+        // //input = "Hello";
+
+        // input = in.nextLine();
+
+        // System.out.println("You typed " + input);
 
     }
 }
